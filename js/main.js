a797e8587edcddd63b73e543f6ea7d3430593216
Artak Chatilyan <artak.chatilyan@gmail.com>
2023-03-17 21:49:38 +0400

    Add animations

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("btn-menu")[0].addEventListener("click", () => {
+  document
+    .getElementsByClassName("panel")[0]
+    .classList.toggle("panel--expanded");
+});
+
+
